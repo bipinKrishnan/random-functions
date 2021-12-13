a0e9603953bd9fafa8f1af5a510145837800743c
Bipin Krishnan <bipinkrishna.p@gmail.com>
2021-12-13 18:53:55 +0530

    added function to seed in pytorch

--- random_functions/pytorch/utils.py
+++ random_functions/pytorch/utils.py
@@ -1,8 +1,14 @@
+import torch 
+import numpy as np
+import os
+import random
+
+
 def check_dataloaders(*args) -> None:
     """
     Prints the shape of the provided PyTorch dataloaders.
 
-    Usage:
+    <u>Usage:</u>
 
     ```python
     from random_functions.pytorch.utils import check_dataloaders
@@ -14,3 +20,30 @@ def check_dataloaders(*args) -> None:
     for i, dl in enumerate(args):
         print(f"Dataloader {i+1}")
         print(next(iter(dl))[0].shape)
+
+def seed_everything(seed: int) -> None:
+    """
+    This function sets the seed value for random number generator, 
+    numpy, torch and cuda(if one is available).
+
+    <u>Usage:</u>
+
+      ```python
+      from random_functions.pytorch.utils import seed_everything
+
+      seed_everything(seed=999)
+      ```
+
+    Args:
+      seed (int): This value is set as the seed for reproducing the results
+
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
--- tests/test_pytorch.py
+++ tests/test_pytorch.py
@@ -1,9 +1,9 @@
-import pytest
-
 import torch
 from torch.utils.data import Dataset, DataLoader
+import random
+import numpy as np
 
-from random_functions.pytorch.utils import check_dataloaders
+from random_functions.pytorch.utils import check_dataloaders, seed_everything
 from random_functions.utils import Capturing
 
 
@@ -34,3 +34,28 @@ def test_check_dataloaders():
     outputs = [eval(i)[0] for i in outputs if "torch.Size" in i]
 
     assert outputs == [bs1, bs2, bs3]
+
+def test_seed_everything():
+    seed = 3
+
+    def get_states(seed):
+        seed_everything(seed)
+        states = [random.getstate(), np.random.get_state()[1], torch.get_rng_state()]
+        if torch.cuda.is_available():
+            states.append(torch.cuda.get_rng_state())
+        return states
+
+    new_states = get_states(seed)
+    new_sec_states = get_states(seed)
+    new_diff_states = get_states(seed+1)
+
+    for new, new_sec, new_diff in zip(*(new_states, new_sec_states, new_diff_states)):
+        if type(new)==torch.Tensor or type(new)==np.ndarray:
+            assert (new == new_sec).all()
+            assert (new != new_diff).any()
+        elif type(new)==tuple:
+            assert new == new_sec
+            assert new != new_diff
+        else:
+            raise ValueError("Unknown datatype found!")
+
